bf5b6fef5caaacfc4651da40fc60925e4738269c
Joseph Hunkeler <jhunkeler@gmail.com>
2017-12-01 21:45:13 -0500

    Consolidate constants, add colors

--- constants.asm
+++ constants.asm
@@ -1,8 +1,12 @@
 %ifndef _CONSTANTS_ASM
 %define _CONSTANTS_ASM
 
+MAX_ROWS equ 25
+MAX_COLS equ 80
 LENGTH_ROW equ 0A0h	; NOTE: length in bytes (80 * 2 = 160)
+LENGTH_COL equ 02h	; NOTE: length in bytes (1 * 2 = 2)
 
+%include "colors.asm"
 %include "ascii.asm"		; ASCII control codes
 %include "scancodes.asm"	; Keyboard scancodes
 
